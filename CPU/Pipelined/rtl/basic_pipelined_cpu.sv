dd17613cb1ec431c5656e8f48584f2037f7a4f57
Tharaka Jayasena <9dmpires2k17.tuj@gmail.com>
2026-09-12 02:32:02 +0530

    cpu pipeline with comb and ff separated

--- CPU/Pipelined/rtl/basic_pipelined_cpu.sv
+++ CPU/Pipelined/rtl/basic_pipelined_cpu.sv
@@ -7,9 +7,10 @@ module cpu (
   output logic [15:0] dmem_wdata,
   output logic        dmem_wen
 );
-  logic [7:0] pc, addr;
+  logic [7:0] pc, addr, pc_temp;
   logic [15:0] regs [16];
   enum logic [3:0] {LOAD, STORE, MOVE, ADD, SUB, MUL, JNZ} opcode;
+  logic [15:0] temp_result_exec, temp_dmem_wdata_mem, temp_result_wb;
 
   typedef struct packed {
     logic [7:0] addr;
@@ -28,7 +29,7 @@ module cpu (
     logic valid;
     logic [7:0] pc_count;
     logic [15:0] instruction;
-  } fetch_reg_t;
+  } fetch_decode_reg_t;
 
   typedef struct packed {
     logic valid;
@@ -37,7 +38,7 @@ module cpu (
     logic [7:0] addr;
     logic [3:0] i_rs1, i_rs2, i_rd, i_reg;
     logic [15:0] reg_1, reg_2;
-  } decode_reg_t;
+  } decode_exe_reg_t;
 
 typedef struct packed {
     logic       valid;
@@ -46,20 +47,21 @@ typedef struct packed {
     logic [7:0] addr;
     logic [15:0] alu_result;
     logic [3:0]  i_rd;
-  } execute_reg_t;
+  } exe_mem_reg_t;
 
 typedef struct packed {
     logic        valid;
+    logic [7:0]  opcode;
     logic [7:0]  pc_count;
-    logic [15:0] data;
+    logic [15:0] alu_result;
+    logic [15:0] mem_data;
     logic [3:0]  i_rd;
-  } memory_reg_t;
+  } mem_wb_reg_t;
 
-
-  fetch_reg_t fetch_reg;
-  decode_reg_t decode_reg;
-  execute_reg_t execute_reg;
-  memory_reg_t memory_reg;
+  fetch_decode_reg_t fetch_decode_reg;
+  decode_exe_reg_t decode_exe_reg;
+  exe_mem_reg_t exe_mem_reg;
+  mem_wb_reg_t mem_wb_reg;
 
   always_ff @(posedge clk) begin
     if (reset) begin
@@ -68,80 +70,127 @@ typedef struct packed {
         regs[i] <= '0;
       end
 
-      fetch_reg <= '{valid: 1'b0, pc_count: '0, instruction: '0};
-      decode_reg <= '{valid: 1'b0, pc_count: '0, opcode: '0, addr: '0};
-      execute_reg <= '{valid: 1'b0, pc_count: '0, opcode: '0, addr: '0, alu_result: '0};
-      memory_reg <= '{valid: 1'b0, pc_count: '0, data: '0};
+      fetch_decode_reg <= '{valid: 1'b0, pc_count: '0, instruction: '0};
+      decode_exe_reg <= '{valid: 1'b0, pc_count: '0, opcode: '0, addr: '0};
+      exe_mem_reg <= '{valid: 1'b0, pc_count: '0, opcode: '0, addr: '0, alu_result: '0};
+      mem_wb_reg <= '{valid: 1'b0, pc_count: '0, alu_result: '0, mem_data: '0, i_rd: '0, opcode: '0};
 
     end else begin
-      if (!fetch_reg.valid) begin
-        fetch_reg <= '{valid: 1'b1, pc_count: pc, instruction: imem_rdata};
-        fetch_reg.valid <= 1'b1;
-        pc <= pc + 1;
-      end
 
-      if (fetch_reg.valid) begin
+      // fetch stage
+      fetch_decode_reg <= '{valid: 1'b1, pc_count: pc, instruction: imem_rdata};
+      fetch_decode_reg.valid <= 1'b1;
+      pc <= pc_temp;
+
+
+      // decode stage
+      if (fetch_decode_reg.valid) begin
 
         instruction_type1_t inst1;
         instruction_type2_t inst2;
 
-        inst1 = instruction_type1_t'(fetch_reg.instruction);
-        inst2 = instruction_type2_t'(fetch_reg.instruction);
+        inst1 = instruction_type1_t'(fetch_decode_reg.instruction);
+        inst2 = instruction_type2_t'(fetch_decode_reg.instruction);
 
-        decode_reg <= '{valid: 1'b1, pc_count: fetch_reg.pc_count, opcode: inst1.opcode, addr: inst1.addr, i_rs1: inst2.i_rs1, i_rs2: inst2.i_rs2, i_rd: inst2.i_rd,
+        decode_exe_reg <= '{valid: 1'b1, pc_count: fetch_decode_reg.pc_count, opcode: inst1.opcode, addr: inst1.addr, i_rs1: inst2.i_rs1, i_rs2: inst2.i_rs2, i_rd: inst2.i_rd,
                           i_reg: inst1.ireg, reg_1: regs[inst2.i_rs1], reg_2: regs[inst2.i_rs2]};
-        decode_reg.valid <= 1'b1;
-        fetch_reg.valid <= 1'b0;
+        decode_exe_reg.valid <= 1'b1;
+        fetch_decode_reg.valid <= 1'b0;
       end
 
-      if (decode_reg.valid) begin
-        logic [15:0] temp_result;
-        case (decode_reg.opcode)
-          LOAD: begin
-            // set adress 
-            temp_result <= dmem_rdata;
-          end
-
-          MOVE: temp_result <= decode_reg.reg_1;
-          ADD : temp_result <= decode_reg.reg_1 + decode_reg.reg_2;
-          SUB : temp_result <= decode_reg.reg_1 - decode_reg.reg_2;
-          MUL : temp_result <= decode_reg.reg_1 * decode_reg.reg_2;
-          JNZ : if (regs[decode_reg.i_reg] != '0) begin
-                  pc <= decode_reg.addr;
-                  decode_reg.valid <= 1'b0;
-                  fetch_reg.valid <= 1'b0;
-                end
+      // EXE: Execute stage
+      if (decode_exe_reg.valid) begin    
+        temp_result_exec = '0;
+        case (decode_exe_reg.opcode)
+          MOVE: temp_result_exec = decode_exe_reg.reg_1;
+          ADD : temp_result_exec = decode_exe_reg.reg_1 + decode_exe_reg.reg_2;
+          SUB : temp_result_exec = decode_exe_reg.reg_1 - decode_exe_reg.reg_2;
+          MUL : temp_result_exec = decode_exe_reg.reg_1 * decode_exe_reg.reg_2;
           default: ;
         endcase
-
-        execute_reg <= '{valid: 1'b1, pc_count: decode_reg.pc_count, opcode: decode_reg.opcode, addr: decode_reg.addr, alu_result: temp_result, i_rd: decode_reg.i_rd};
-        execute_reg.valid <= 1'b1;
-        decode_reg.valid <= 1'b0;
+        exe_mem_reg <= '{valid: 1'b1, pc_count: decode_exe_reg.pc_count, opcode: decode_exe_reg.opcode, addr: decode_exe_reg.addr, alu_result: temp_result_exec, i_rd: decode_exe_reg.i_rd};
+        exe_mem_reg.valid <= 1'b1;
+        decode_exe_reg.valid <= 1'b0;
       end
 
-      if (execute_reg.valid) begin
-        if (execute_reg.opcode == ADD) begin // include other operations as well
-          regs[execute_reg.i_rd] <= execute_reg.alu_result;
-        end
+      // MEM:
+      if (exe_mem_reg.valid) begin
 
-        memory_reg <= '{valid: 1'b1, pc_count: execute_reg.pc_count, data: execute_reg.alu_result, i_rd: execute_reg.i_rd};
-        memory_reg.valid <= 1'b1;
-        execute_reg.valid <= 1'b0;
+        // load or store
+        dmem_wdata <= temp_dmem_wdata_mem;
+
+        mem_wb_reg <= '{valid: 1'b1, pc_count: exe_mem_reg.pc_count, opcode: exe_mem_reg.opcode, alu_result: exe_mem_reg.alu_result, i_rd: exe_mem_reg.i_rd, mem_data: dmem_rdata};
+        mem_wb_reg.valid <= 1'b1;
+        exe_mem_reg.valid <= 1'b0;
       end
 
-      if (memory_reg.valid) begin
-        regs[memory_reg.i_rd] <= memory_reg.data;
-        memory_reg.valid <= 1'b0;
+      // WB :
+      if (mem_wb_reg.valid) begin
+        // write back to register file
+        regs[mem_wb_reg.i_rd] <= temp_result_wb;
+        mem_wb_reg.valid <= 1'b0;
       end
     end
   end
 
-  always_comb begin
+  // Fetch:
+  always_comb begin : FETCH_COMB
     imem_addr = pc;
-
-    dmem_addr  = addr;
-    dmem_wdata = regs[i_reg];
-    dmem_wen   = !reset && opcode == STORE;
   end
+
+  // Decode:
+  always_comb begin : DECODE_COMB
+    pc_temp = pc + 1'b1;
+
+    if(fetch_decode_reg.opcode == JNZ) begin
+      instruction_type1_t inst1;
+      inst1 = instruction_type1_t'(fetch_decode_reg.instruction);
+      if (regs[inst1.ireg] != '0) begin
+        pc_temp = inst1.addr;
+      end
+    end
+  end
+
+
+  //Exec:
+  always_comb begin EXEC_COMB
+    case (decode_exe_reg.opcode)
+      MOVE: temp_result_exec = decode_exe_reg.reg_1;
+      ADD : temp_result_exec = decode_exe_reg.reg_1 + decode_exe_reg.reg_2;
+      SUB : temp_result_exec = decode_exe_reg.reg_1 - decode_exe_reg.reg_2;
+      MUL : temp_result_exec = decode_exe_reg.reg_1 * decode_exe_reg.reg_2;
+      default: ;
+    endcase
+  end
+
+
+  // MEM: 
+  always_comb begin MEM_COMB
+    dmem_addr = '0;
+    dmem_wen   = 1'b0;
+    temp_dmem_wdata_mem = '0;
+    if (exe_mem_reg.opcode == LOAD) begin
+      dmem_addr = exe_mem_reg.addr;
+    end else if (exe_mem_reg.opcode == STORE) begin
+      dmem_addr = exe_mem_reg.addr;
+      dmem_wen   = 1'b1;
+      temp_dmem_wdata_mem = regs[exe_mem_reg.i_rd];
+    end
+  end
+
+  // WB:
+  always_comb begin WB_COMB
+    temp_result_wb = '0;
+    case (mem_wb_reg.opcode)
+      LOAD: temp_result_wb = mem_wb_reg.mem_data;
+      MOVE: temp_result_wb = mem_wb_reg.alu_result;
+      ADD : temp_result_wb = mem_wb_reg.alu_result;
+      SUB : temp_result_wb = mem_wb_reg.alu_result;
+      MUL : temp_result_wb = mem_wb_reg.alu_result;
+      default: ;
+    endcase
+  end
+  
+    
   
 endmodule
