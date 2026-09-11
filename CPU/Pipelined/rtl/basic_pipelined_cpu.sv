f07e02b095cb4da8ee6c0e9ac57663d1836d9e81
Tharaka Jayasena <9dmpires2k17.tuj@gmail.com>
2026-09-11 16:46:39 +0530

    v1 almost done

--- CPU/Pipelined/rtl/basic_pipelined_cpu.sv
+++ CPU/Pipelined/rtl/basic_pipelined_cpu.sv
@@ -8,7 +8,21 @@ module cpu (
   output logic        dmem_wen
 );
   logic [7:0] pc, addr;
+  logic [15:0] regs [16];
   enum logic [3:0] {LOAD, STORE, MOVE, ADD, SUB, MUL, JNZ} opcode;
+
+  typedef struct packed {
+    logic [7:0] addr;
+    logic [3:0] ireg;
+    logic [3:0] opcode;
+  } instruction_type1_t;
+
+  typedef struct packed {
+    logic [3:0] i_rs1;
+    logic [3:0] i_rs2;
+    logic [3:0] i_rd;
+    logic [3:0] opcode;
+  } instruction_type2_t;
 
   typedef struct packed {
     logic valid;
@@ -22,20 +36,30 @@ module cpu (
     logic [3:0] opcode;
     logic [7:0] addr;
     logic [3:0] i_rs1, i_rs2, i_rd, i_reg;
-
+    logic [15:0] reg_1, reg_2;
   } decode_reg_t;
 
-  typedef struct packed {
-    logic valid;
+typedef struct packed {
+    logic       valid;
     logic [7:0] pc_count;
     logic [3:0] opcode;
     logic [7:0] addr;
+    logic [15:0] alu_result;
+    logic [3:0]  i_rd;
   } execute_reg_t;
+
+typedef struct packed {
+    logic        valid;
+    logic [7:0]  pc_count;
+    logic [15:0] data;
+    logic [3:0]  i_rd;
+  } memory_reg_t;
 
 
   fetch_reg_t fetch_reg;
   decode_reg_t decode_reg;
   execute_reg_t execute_reg;
+  memory_reg_t memory_reg;
 
   always_ff @(posedge clk) begin
     if (reset) begin
@@ -46,50 +70,81 @@ module cpu (
 
       fetch_reg <= '{valid: 1'b0, pc_count: '0, instruction: '0};
       decode_reg <= '{valid: 1'b0, pc_count: '0, opcode: '0, addr: '0};
+      execute_reg <= '{valid: 1'b0, pc_count: '0, opcode: '0, addr: '0, alu_result: '0};
+      memory_reg <= '{valid: 1'b0, pc_count: '0, data: '0};
 
     end else begin
       if (!fetch_reg.valid) begin
         fetch_reg <= '{valid: 1'b1, pc_count: pc, instruction: imem_rdata};
+        fetch_reg.valid <= 1'b1;
+        pc <= pc + 1;
       end
 
       if (fetch_reg.valid) begin
-        decode_reg <= '{valid: 1'b1, pc_count: fetch_reg.pc_count, opcode: fetch_reg.instruction[15:12], addr: fetch_reg.instruction[7:0]};
+
+        instruction_type1_t inst1;
+        instruction_type2_t inst2;
+
+        inst1 = instruction_type1_t'(fetch_reg.instruction);
+        inst2 = instruction_type2_t'(fetch_reg.instruction);
+
+        decode_reg <= '{valid: 1'b1, pc_count: fetch_reg.pc_count, opcode: inst1.opcode, addr: inst1.addr, i_rs1: inst2.i_rs1, i_rs2: inst2.i_rs2, i_rd: inst2.i_rd,
+                          i_reg: inst1.ireg, reg_1: regs[inst2.i_rs1], reg_2: regs[inst2.i_rs2]};
+        decode_reg.valid <= 1'b1;
         fetch_reg.valid <= 1'b0;
+      end
+
+      if (decode_reg.valid) begin
+        
+
+        execute_reg <= '{valid: 1'b1, pc_count: decode_reg.pc_count, opcode: decode_reg.opcode, addr: decode_reg.addr, alu_result: temp_result, i_rd: decode_reg.i_rd};
+        execute_reg.valid <= 1'b1;
+        decode_reg.valid <= 1'b0;
+      end
+
+      if (execute_reg.valid) begin
+        if (execute_reg.opcode == ADD) begin // include other operations as well
+          regs[execute_reg.i_rd] <= execute_reg.alu_result;
+        end
+
+        memory_reg <= '{valid: 1'b1, pc_count: execute_reg.pc_count, data: execute_reg.alu_result, i_rd: execute_reg.i_rd};
+        memory_reg.valid <= 1'b1;
+        execute_reg.valid <= 1'b0;
+      end
+
+      if (memory_reg.valid) begin
+        regs[memory_reg.i_rd] <= memory_reg.data;
+        memory_reg.valid <= 1'b0;
       end
     end
   end
 
-  // always_comb begin
-  //   imem_addr = pc;
-  //   {addr        , i_reg, opcode} = imem_rdata;
-  //   {i_rs2, i_rs1, i_rd , opcode} = imem_rdata;
+  always_comb begin
+    imem_addr = pc;
 
-  //   dmem_addr  = addr;
-  //   dmem_wdata = regs[i_reg];
-  //   dmem_wen   = !reset && opcode == STORE;
+    dmem_addr  = addr;
+    dmem_wdata = regs[i_reg];
+    dmem_wen   = !reset && opcode == STORE;
+  end
 
-  //   reg_1      = regs[i_rs1];
-  //   reg_2      = regs[i_rs2];
-  // end
-
-  always_ff @(posedge clk)
-    if (reset) begin
-      pc   <= '0;
-      foreach (regs[i]) begin
-        regs[i] <= '0;
-      end
-    end else begin
-      pc   <= pc + 1'b1;
-
-      case (opcode)
-        LOAD: regs[i_reg] <= dmem_rdata;
-        MOVE: regs[i_rd ] <= reg_1;
-        ADD : regs[i_rd ] <= reg_1 + reg_2;
-        SUB : regs[i_rd ] <= reg_1 - reg_2;
-        MUL : regs[i_rd ] <= reg_1 * reg_2;
-        JNZ : if (regs[i_reg] != '0) pc <= addr;
-        default: ;
-      endcase
-    end
-
+  always_comb begin : execution_stage
+    logic [15:0] temp_result;
+        case (decode_reg.opcode)
+          LOAD: begin
+            // set adress 
+            temp_result <= dmem_rdata;
+          end
+          MOVE: temp_result <= decode_reg.reg_1;
+          ADD : temp_result <= decode_reg.reg_1 + decode_reg.reg_2;
+          SUB : temp_result <= decode_reg.reg_1 - decode_reg.reg_2;
+          MUL : temp_result <= decode_reg.reg_1 * decode_reg.reg_2;
+          JNZ : if (regs[decode_reg.i_reg] != '0) begin
+                  pc <= decode_reg.addr;
+                  decode_reg.valid <= 1'b0;
+                  fetch_reg.valid <= 1'b0;
+                end
+          default: ;
+        endcase
+  end
+  
 endmodule
